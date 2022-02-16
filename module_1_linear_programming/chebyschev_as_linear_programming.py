ff6b72dcd33d3de42633d36b3b85c2e432b7b3cd
Exr0n <mail@exr0n.com>
2022-02-15 21:32:31 -0800

    ack diagonal distance not working

--- module_1_linear_programming/chebyschev_as_linear_programming.py
+++ module_1_linear_programming/chebyschev_as_linear_programming.py
@@ -10,6 +10,8 @@ M = 3   # number of constraints
 import numpy as np
 from scipy.optimize import linprog
 
+from math import sqrt
+
 # variable meanings in linprog
 # X : Nx1
 # C : 1xN
@@ -28,22 +30,30 @@ def convert_convex_objective(c, c_b, A_ub, b_ub, A_eq, b_eq, bounds=[], x_0=None
 
     # minimize z subject to z.broadcast() <= (c @ x + c_b), A_eq @ x = b_eq, A_ub @ x <= b_ub
     # lets tack on z to the end
-    neo_c = np.array([[0]*c.shape[0] + [1]])
+    neo_c = np.array([[0]*c.shape[1] + [1]])
     # neo_A_ub = np.concatenate((A_ub, np.array([0] * A_ub.shape[1])), axis=1)    # need vertical concat for the maximize constraints
     neo_A_ub = np.zeros([A_ub.shape[0] + c.shape[0], A_ub.shape[1] + 1])
     neo_A_ub[:A_ub.shape[0], :A_ub.shape[1]] = A_ub
+    neo_A_ub[A_ub.shape[0]:, :A_ub.shape[1]] = c
+    neo_A_ub[A_ub.shape[0]:, -1] = [-1] * c.shape[0]
 
-    neo_b_ub = # TODO
+    neo_b_ub = np.expand_dims(np.concatenate([b_ub, -c_b]), 0)
 
     print('\n\nc\n', c, '\n\n', neo_c)
     print('\n\nA_ub\n', A_ub, '\n\n', neo_A_ub)
 
-    return
+    print('neo_b_ub:\n', neo_b_ub)
+
+    neo_A_eq = np.zeros([A_eq.shape[0], A_eq.shape[1] + 1])
+    neo_A_eq[:A_eq.shape[0], :A_eq.shape[1]] = A_eq
+    neo_b_eq = np.concatenate([b_eq, [0]], 0)
+
+    # print(c, neo_A_eq)
 
     if bounds == []:
-        return linprog(neo_c, neo_A_ub, neo_b_ub, A_eq, b_eq)
+        return linprog(neo_c, neo_A_ub, neo_b_ub, neo_A_eq, neo_b_eq)
     else:
-        return linprog(neo_c, neo_A_ub, neo_b_ub, A_eq, b_eq, bounds)
+        return linprog(neo_c, neo_A_ub, neo_b_ub, neo_A_eq, neo_b_eq, bounds)
 
 if __name__ == '__main__':
     A_ub = np.array([[-1,  0],
@@ -51,9 +61,11 @@ if __name__ == '__main__':
                      [ 1,  1]])
     b_ub = np.array([0, 0, 2])
 
+    # get the minumum of this matrix as the objective fn, then maximize it
     c = np.array([[1, 0],
                   [0, 1],
-                  [9, 9]])
-    c_b = np.array([0, 0, 0])
+                  [-sqrt(2), -sqrt(2)]])
+    c_b = np.array([0, 0, 2*sqrt(2)])
 
-    convert_convex_objective(c, c_b, A_ub, b_ub, np.array([[]]), np.array([[]]))
+    got = convert_convex_objective(c, c_b, A_ub, b_ub, np.array([[0]*c.shape[1]]), np.array([]))
+    print(got)
