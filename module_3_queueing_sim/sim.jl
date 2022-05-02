e2f92d07a12d5727d5d4e9f8a2fc52bbce559849
Exr0n <mail@exr0n.com>
2022-05-02 12:40:11 -0700

    prioritiy queue not prioritizing

--- module_3_queueing_sim/sim.jl
+++ module_3_queueing_sim/sim.jl
@@ -1,8 +1,9 @@
 println("compiled... executing")
 using Distributions, Random
-using Plots; gr()
-using DataStructures
+using Plots; gr();
 using Match
+using DataStructures
+# using Base.Collections
 using Formatting    # https://stackoverflow.com/a/37032242
 
 SEED = 1338
@@ -23,66 +24,83 @@ function simulate(n_customers)
     println(round.(samples_arrive, digits=1))
     println(round.(samples_checko, digits=1))
 
-# samples_arrive, samples_checko = floor.(samples_arrive), floor.(samples_checko)
+    samples_arrive, samples_checko = floor.(samples_arrive), floor.(samples_checko)
 
     samples_arrive  = cumsum(samples_arrive)                        # T_k
     # wait_cumsum = zeros(n_customers + 1)
     # wait_cumsum[2:end] = cumsum(samples_checko)                     # leaving time
 
+    
+    # SHOPPING_TIME = 15;
+    SHOPPING_TIME = 0;
+
+
+    # state
+    queue = []
+    n_available_cashiers = 1
+
     events = PriorityQueue()
     for (i, t) in enumerate(samples_arrive)
-        enqueue!(events, t=>("arrived", i))
+        enqueue!(events, t, ("arrived", i))
     end 
 
+    # metrics
     len_on_enter = zeros(Int, n_customers)
-
-    queue = []
     wait_time_by_customer = zeros(n_customers)
+    event_times_by_customer = zeros(n_customers, 4)     # events: 1 = arrive, 2 = queue, 3 = dequeue, 4 = done serving
 
     # event loop
     while length(events) > 0
         (t, (ev, id)) = dequeue_pair!(events)
         printfmtln("at time {:.2f}, {} {}", t, id, ev)
         @match ev begin
-            "arrived" => (  enqueue!(events, t + 15 => ("queued", id))
+            "arrived" => (  event_times_by_customer[id, 1] = t;
+                            println("    queued getting in line event");
+                            println("peek: ", peek(events));
+                            enqueue!(events, t + SHOPPING_TIME, ("queued", id))
                         )
-            "queued"  => (  len_on_enter[id] = length(queue);
+            "queued"  => (  event_times_by_customer[id, 2] = t;
+                            len_on_enter[id] = length(queue);
                             # len_on_enter[id] = line_len;
                             println("    with $(length(queue)) people in line");
                             wait_time_by_customer[id] = t;
 
                             push!(queue, id);
-                            if length(queue) == 0
-                                enqueue!(events, t => ("dequeued", id))
+                            if n_available_cashiers > 0
+                                println("    the line was empty!")
+                                enqueue!(events, t, ("dequeued", id))
                             end
-
-                            # if length(queue) > 0
-                            #     push!(queue, id);
-                            # else
-                            #     enqueue!(events, t => ("dequeued", id))
-                            # end 
-
-
-                            # println("    waiting $(wait_cumsum[id] - t)");
-                            # wait_time = wait_cumsum[id] - t;
-                            # enqueue!(events, t + wait_time + samples_checko[id] => ("dequeued", id));
-                            # line_len += 1;
                         )
-            "dequeued" => ( wait_time_by_customer[id] = t-wait_time_by_customer[id];
+            "dequeued" => ( event_times_by_customer[id, 3] = t;
+                            wait_time_by_customer[id] = t-wait_time_by_customer[id];
                             @assert id == popfirst!(queue);
-                            enqueue!(events, t + samples_checko
-                                                 => ("done_serving", id))
-                    )
+                            println("    after waiting $(wait_time_by_customer[id]) mins");
+                            enqueue!(events, t + samples_checko[id], ("done_serving", id));
+                            n_available_cashiers -= 1;
+                        )
             "done_serving" => (
+                            event_times_by_customer[id, 4] = t;
+                            n_available_cashiers += 1;
                             if length(queue) > 0
-                                enqueue!(events, t => ("dequeued", queue[1]))
+                                enqueue!(events, t, ("dequeued", queue[1]))
                             end 
-                    )
+                        )
             unk => println("- unknown event $unk")
         end
     end
 
-    println(len_on_enter)
+    # post processing metrics
+    # pushfirst!(wait_time_by_customer, zeros(n_customers))
+    println(event_times_by_customer);
+    # plot(map(x -> (x, 1:4)))
+    for customer in 1:n_customers
+        println("plotting customer number $customer")
+        plot!(event_times_by_customer[customer, :], 1:4, linetype=:steppre);
+    end
+    gui();
+    readline();
+
+    println(len_on_enter);
 end
 
 simulate(CUSTOMERS)
